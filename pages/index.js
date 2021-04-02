1ac151e9b81cb099fd1e258121870dcc161d3e0f
samx23 <skalexsong@gmail.com>
2021-04-02 19:39:53 +0700

    add basic fetching to only fetch surah no.1

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
-import { randomNum } from "../src/randomNum";
+import { useEffect, useState } from "react";
+import { getAyah } from "../src/data";
+import { randomSurah, randomAyah } from "../src/randomNum";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const URL = "https://api.quran.com/api/v4";
-  const get = fetch(URL);
+  const [text, setText] = useState({});
+  const ayah = () => randomAyah(1, 9);
+  const get = getAyah(1, ayah());
+  useEffect(() => {
+    fetch(get)
+      .then((response) => response.json())
+      .then((data) => setText(data))
+      .catch((error) => console.log("Fetch error : "));
+  }, []);
 
-  console.log(get);
-
+  console.log(text);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,19 +24,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>{randomNum(1, 114)}</h1>
+        <h1>{text.status === "OK." ? text.data.text.arab : null}</h1>
+        <h2>{text.status === "OK." ? text.data.translation.id : null}</h2>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
