28a99a945aa890aa2f815c1d1ed8cff6403c065b
samx23 <skalexsong@gmail.com>
2021-04-02 19:39:25 +0700

    Create randomizer function for ayah and surah

---
+++ src/randomNum.js
@@ -0,0 +1,15 @@
+export const randomSurah = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const randomAyah = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// 6.236 ayah
+// 114 surah
+// 30 juz
