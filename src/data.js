bc9ec119424b4d2a288e25313b43132a71fd0fbc
samx23 <skalexsong@gmail.com>
2021-04-03 13:46:35 +0700

    fix url source

--- src/data.js
+++ src/data.js
@@ -1,2 +1,5 @@
-const URL = "https://api.quran.sutanlab.id/surah";
-export const getAyah = (surah, ayah) => `${URL}/${surah}/${ayah}`;
+const URLSutanlab = "https://api.quran.sutanlab.id/surah";
+const URLAlquranCloud = "http://api.alquran.cloud/v1/surah";
+
+export const getTranslate = (surah, ayah) => `${URLSutanlab}/${surah}/${ayah}`;
+export const getAyah = (surah) => `${URLAlquranCloud}/${surah}`;
