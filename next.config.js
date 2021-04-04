f53c11fa85dda55997623294167a9f6153a165f3
samx23 <skalexsong@gmail.com>
2021-04-04 08:50:57 +0700

    add next configuration

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+  images: {
+    domains: ["cdn.alquran.cloud"],
+  },
+});
