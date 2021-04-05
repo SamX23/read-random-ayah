d9de1d51c18da328ca04af90bcb16d5498a2cbf5
samx23 <skalexsong@gmail.com>
2021-04-05 18:17:53 +0700

    remove unused js and css

--- src/data.js
+++ src/data.js
@@ -1,8 +1,5 @@
 const URLSutanlab = "https://api.quran.sutanlab.id/surah";
 const URLAlquranCloud = "https://api.alquran.cloud/v1/surah";
-const CDNAlquranCloud = "https://cdn.alquran.cloud/media/image";
 
 export const getTranslate = (surah, ayah) => `${URLSutanlab}/${surah}/${ayah}`;
 export const getAyah = (surah) => `${URLAlquranCloud}/${surah}`;
-export const getAyahCdn = (surah, ayah) =>
-  `${CDNAlquranCloud}/${surah}/${ayah}`;
