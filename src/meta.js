c658691362656a3846a82267c22ad3ef8493d0db
samx23 <skalexsong@gmail.com>
2021-04-05 18:36:58 +0700

    update

--- src/meta.js
+++ src/meta.js
@@ -35,7 +35,7 @@ const Meta = () => {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="manifest" href="/manifest.json" />
       <link
-        rel="preload"
+        rel="prefetch"
         href="/Assets/font/me_quran_volt_newmet.ttf"
         as="font"
       />
