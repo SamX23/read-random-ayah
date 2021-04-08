eb029b4f16412cfb920f53f90359b7dd08868471
samx23 <skalexsong@gmail.com>
2021-04-08 07:57:55 +0700

    First setup of chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
+import { Chakra } from "../src/Chakra";
 import Meta from "../src/meta";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Chakra>
       <Meta />
       <Component {...pageProps} />
-    </>
+    </Chakra>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static getInitialProps(ctx) {
+    return NextDocument.getInitialProps(ctx);
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head />
         <body>
+          <ColorModeScript initialColorMode="system" />
           <Main />
           <NextScript />
         </body>
