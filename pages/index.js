f253f96b614f8b6032ff87a765b0fb8c745844e6
samx23 <skalexsong@gmail.com>
2021-04-14 11:40:01 +0700

    Update : use data layer from reducer

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,68 @@
-import { useEffect, useState } from "react";
-import Nav from "../components/nav";
+import { useEffect } from "react";
+import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 import Surah from "../components/surah";
 import SurahDescription from "../components/surahDescription";
 import { useGlobalState } from "../utils/context";
 import { randomAyah } from "../utils/randomNum";
+import { randomFetch } from "../utils/fetch";
 
 export default function Home() {
-  const result = useGlobalState();
-  const [numberAyah, setNumberAyah] = useState(0);
-  const { data, status } = result;
+  const [{ dataSource, currentAyah }, dispatch] = useGlobalState();
+  const { data, status } = dataSource;
   const valid = status === "OK";
-
   const { numberOfAyahs, number, ayahs } = valid && data;
   const maxAyah = valid && numberOfAyahs;
   const surah = valid && number;
-  const ayah = valid && ayahs[numberAyah];
+  const ayah = valid && ayahs[currentAyah];
 
   useEffect(() => {
-    setNumberAyah(randomAyah(0, maxAyah));
-  }, [result]);
+    let mounted = true;
+    randomFetch().then(
+      (result) =>
+        mounted &&
+        dispatch({
+          type: "SET_RANDOM_DATA",
+          dataSource: result,
+        })
+    );
+
+    return function cleanup() {
+      mounted = false;
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_AYAH",
+      currentAyah: randomAyah(0, maxAyah),
+    });
+  }, [dataSource]);
 
   const handleNext = () => {
-    if (numberAyah !== numberOfAyahs - 1) {
-      setNumberAyah((noAyah) => noAyah + 1);
+    if (currentAyah !== numberOfAyahs - 1) {
+      dispatch({
+        type: "NEXT_AYAH",
+        currentAyah: currentAyah + 1,
+      });
     }
   };
 
   const handlePrevious = () => {
-    if (numberAyah !== 0) {
-      setNumberAyah((noAyah) => noAyah - 1);
+    if (currentAyah !== 0) {
+      dispatch({
+        type: "PREVIOUS_AYAH",
+        currentAyah: currentAyah - 1,
+      });
     }
   };
 
+  console.log("data :", dataSource);
+  console.log("current ayah :", currentAyah);
+  console.log("max ayah:", maxAyah);
+
   return (
-    <>
-      <Nav />
+    <Layout>
       <main className={styles.container}>
         <div className={styles.main}>
           <div className={styles.bismillah}>
@@ -66,6 +93,6 @@ export default function Home() {
           )}
         </div>
       </main>
-    </>
+    </Layout>
   );
 }
