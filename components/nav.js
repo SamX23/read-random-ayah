23f9f5bb54dfbd5d8549df59579148ca464bc90a
samx23 <skalexsong@gmail.com>
2021-04-14 11:38:33 +0700

    Update : fix eslint error

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import styles from "../styles/Layout.module.css";
 
-const Nav = () => {
+const Nav = ({ styles }) => {
   const [show, setShow] = useState(true);
   const handleClick = () => {
     setShow(!show);
@@ -10,19 +9,17 @@ const Nav = () => {
   return (
     <header className={styles.navigation}>
       <div className={styles.sidebar_Button}>
-        <span onClick={() => handleClick()}>☰</span>
+        <button type="button" onClick={() => handleClick()}>
+          ☰
+        </button>
       </div>
       {!show && (
         <div className={styles.sidebar}>
           <div className={styles.menu}>
             <h1>MENU</h1>
             <ul>
-              <li>
-                <a>HOME</a>
-              </li>
-              <li>
-                <a>QURAN</a>
-              </li>
+              <li>HOME</li>
+              <li>QURAN</li>
               <li>
                 <a href="https://github.com/SamX23/read-random-ayah">ABOUT</a>
               </li>
