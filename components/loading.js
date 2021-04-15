51313b7020d0f6ab586ef45dcc49eef1c309aaa8
samx23 <skalexsong@gmail.com>
2021-04-15 12:59:15 +0700

    Update : Add indicator on loading component

--- components/loading.js
+++ components/loading.js
@@ -1,6 +1,10 @@
+import styles from "../styles/Layout.module.css";
+
 const Loading = () => (
-  <div>
-    <h1>please wait.. :)</h1>
+  <div className={styles.loading}>
+    <div className={styles.bouncingLoader}>
+      <div />
+    </div>
   </div>
 );
 
