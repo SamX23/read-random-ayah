93dc07c3f009e69cc0f2c5165d11a930a67c0bb2
samx23 <skalexsong@gmail.com>
2021-04-17 02:12:38 +0700

    Update : move bismillah section to ayah component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
+import randomFetch from "../utils/fetch";
 import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 import SurahLayout from "../components/randomSurah/layout";
 import { useGlobalState } from "../utils/context";
-import { randomFetch } from "../utils/fetch";
 
 export default function Home() {
-  // eslint-disable-next-line no-empty-pattern
   const [{ dataSource }, dispatch] = useGlobalState();
 
   useEffect(() => {
@@ -29,11 +28,6 @@ export default function Home() {
     <Layout>
       <main className={styles.container}>
         <div className={styles.main}>
-          <div className={styles.bismillah}>
-            <h1 className={styles.ayah}>
-              بِسْمِ ٱللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
-            </h1>
-          </div>
           {dataSource && <SurahLayout styles={styles} />}
         </div>
       </main>
