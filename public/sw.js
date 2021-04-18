5295e89e1f308a129d181a494a6916bf0870a526
samx23 <skalexsong@gmail.com>
2021-04-18 21:12:53 +0700

    Update generated SW

--- public/sw.js
+++ public/sw.js
@@ -1,116 +1,302 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// If the loader is already loaded, just stop.
 if (!self.define) {
-  const singleRequire = name => {
-    if (name !== 'require') {
-      name = name + '.js';
-    }
-    let promise = Promise.resolve();
-    if (!registry[name]) {
-      
-        promise = new Promise(async resolve => {
+  const e = (e) => {
+    e !== "require" && (e += ".js");
+    let i = Promise.resolve();
+    return (
+      n[e] ||
+        (i = new Promise(async (i) => {
           if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = name;
-            document.head.appendChild(script);
-            script.onload = resolve;
-          } else {
-            importScripts(name);
-            resolve();
-          }
+            const n = document.createElement("script");
+            (n.src = e), document.head.appendChild(n), (n.onload = i);
+          } else importScripts(e), i();
+        })),
+      i.then(() => {
+        if (!n[e]) throw new Error(`Module ${e} didn’t register its module`);
+        return n[e];
+      })
+    );
+  };
+  const i = (i, n) => {
+    Promise.all(i.map(e)).then((e) => n(e.length === 1 ? e[0] : e));
+  };
+  const n = { require: Promise.resolve(i) };
+  self.define = (i, s, c) => {
+    n[i] ||
+      (n[i] = Promise.resolve().then(() => {
+        const n = {};
+        const r = { uri: location.origin + i.slice(1) };
+        return Promise.all(
+          s.map((i) => {
+            switch (i) {
+              case "exports":
+                return n;
+              case "module":
+                return r;
+              default:
+                return e(i);
+            }
+          })
+        ).then((e) => {
+          const i = c(...e);
+          return n.default || (n.default = i), n;
         });
-      
-    }
-    return promise.then(() => {
-      if (!registry[name]) {
-        throw new Error(`Module ${name} didn’t register its module`);
-      }
-      return registry[name];
-    });
-  };
-
-  const require = (names, resolve) => {
-    Promise.all(names.map(singleRequire))
-      .then(modules => resolve(modules.length === 1 ? modules[0] : modules));
-  };
-  
-  const registry = {
-    require: Promise.resolve(require)
-  };
-
-  self.define = (moduleName, depsNames, factory) => {
-    if (registry[moduleName]) {
-      // Module is already loading or loaded.
-      return;
-    }
-    registry[moduleName] = Promise.resolve().then(() => {
-      let exports = {};
-      const module = {
-        uri: location.origin + moduleName.slice(1)
-      };
-      return Promise.all(
-        depsNames.map(depName => {
-          switch(depName) {
-            case "exports":
-              return exports;
-            case "module":
-              return module;
-            default:
-              return singleRequire(depName);
-          }
-        })
-      ).then(deps => {
-        const facValue = factory(...deps);
-        if(!exports.default) {
-          exports.default = facValue;
-        }
-        return exports;
-      });
-    });
+      }));
   };
 }
-define("./sw.js",['./workbox-36aad3af'], function (workbox) { 'use strict';
-
-  /**
-  * Welcome to your Workbox-powered service worker!
-  *
-  * You'll need to register this file in your web app.
-  * See https://goo.gl/nhQhGp
-  *
-  * The rest of the code is auto-generated. Please don't update this file
-  * directly; instead, make changes to your Workbox build configuration
-  * and re-run your build process.
-  * See https://goo.gl/2aRDsh
-  */
-
-  importScripts();
-  self.skipWaiting();
-  workbox.clientsClaim();
-  workbox.registerRoute("/", new workbox.NetworkFirst({
-    "cacheName": "start-url",
-    "networkTimeoutSeconds": 10,
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 1,
-      maxAgeSeconds: 86400,
-      purgeOnQuotaError: true
-    })]
-  }), 'GET');
-  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
-    "cacheName": "dev",
-    plugins: []
-  }), 'GET');
-
+define("./sw.js", ["./workbox-030153e1"], (e) => {
+  importScripts(),
+    self.skipWaiting(),
+    e.clientsClaim(),
+    e.precacheAndRoute(
+      [
+        {
+          url: "/Assets/font/me_quran_volt_newmet.ttf",
+          revision: "a79b204e9c3055c77f0d81921bd881c2",
+        },
+        {
+          url: "/_next/static/chunks/commons.b33d69fee4c480cb880f.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url:
+            "/_next/static/chunks/f6078781a05fe1bcb0902d23dbbb2662c8d200b3.b110b24f71599df941c8.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/framework.4b81eedf2fcdb09bf521.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/main-a53dd5f973d5f0c87205.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/pages/_app-83db32f604988d8318b9.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/pages/_error-b2ff4627d1ca4a0ecaab.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/pages/index-58736103328163a2b06d.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/polyfills-4f14e8c8ea1352d3ef0d.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/chunks/webpack-50bee04d1dc61f8adf5b.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/css/6e9ef204d6fd7ac61493.css",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/css/dbb73b85d7969168b718.css",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/fcSyc6Kil-nyNh-iqIpM5/_buildManifest.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/_next/static/fcSyc6Kil-nyNh-iqIpM5/_ssgManifest.js",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url:
+            "/_next/static/media/me_quran_volt_newmet.0df26babbd330ac290ae8891eb1a3fb3.ttf",
+          revision: "fcSyc6Kil-nyNh-iqIpM5",
+        },
+        {
+          url: "/android-chrome-192x192.png",
+          revision: "fe42b36735a46ae47095a7c0c744d2e2",
+        },
+        {
+          url: "/android-chrome-512x512.png",
+          revision: "87b19d98a6ecd0eab78d9ea84f89f3be",
+        },
+        {
+          url: "/apple-touch-icon-120x120.png",
+          revision: "07cf42a179dbefbbbb6cab1284598b7f",
+        },
+        {
+          url: "/apple-touch-icon-152x152.png",
+          revision: "fb2ec793785451b1fbf6c1e11639442b",
+        },
+        {
+          url: "/apple-touch-icon-180x180.png",
+          revision: "f129e89b5e20476bf4dc82a901d7e245",
+        },
+        {
+          url: "/apple-touch-icon-60x60.png",
+          revision: "47ff4c10aba2448eb3ac54082fe6035a",
+        },
+        {
+          url: "/apple-touch-icon-76x76.png",
+          revision: "db6c55c2fb81623080a60903c55a5fa2",
+        },
+        {
+          url: "/apple-touch-icon.png",
+          revision: "f129e89b5e20476bf4dc82a901d7e245",
+        },
+        {
+          url: "/browserconfig.xml",
+          revision: "61bfd064535af0c276bb63b3fd579733",
+        },
+        {
+          url: "/favicon-16x16.png",
+          revision: "4c00dc4edf2d9ff4c21d6b153a661647",
+        },
+        {
+          url: "/favicon-32x32.png",
+          revision: "20b7e1bb5fd3f6167ca41b31579baeb9",
+        },
+        { url: "/favicon.ico", revision: "3f372eae62736ef4add7a26b22ff2980" },
+        { url: "/manifest.json", revision: "e77661d99698f4418adb7d91716a93c0" },
+        {
+          url: "/mstile-150x150.png",
+          revision: "9835159c62a42c6304c46499ae927b6f",
+        },
+        {
+          url: "/safari-pinned-tab.svg",
+          revision: "12f108fba66af1a237d4fb23023c5d35",
+        },
+        { url: "/vercel.svg", revision: "4b4f1876502eb6721764637fe5c41702" },
+      ],
+      { ignoreURLParametersMatching: [] }
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      "/",
+      new e.NetworkFirst({
+        cacheName: "start-url",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 1,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      new e.CacheFirst({
+        cacheName: "google-fonts",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 4,
+            maxAgeSeconds: 31536e3,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-font-assets",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 4,
+            maxAgeSeconds: 604800,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-image-assets",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 64,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:js)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-js-assets",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:css|less)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-style-assets",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:json|xml|csv)$/i,
+      new e.NetworkFirst({
+        cacheName: "static-data-assets",
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\/api\/.*$/i,
+      new e.NetworkFirst({
+        cacheName: "apis",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 16,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /.*/i,
+      new e.NetworkFirst({
+        cacheName: "others",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400,
+            purgeOnQuotaError: !0,
+          }),
+        ],
+      }),
+      "GET"
+    );
 });
-//# sourceMappingURL=sw.js.map
