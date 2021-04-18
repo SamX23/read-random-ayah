a5c96eebccdcdc7809f51c4dff14e34e44f96070
samx23 <skalexsong@gmail.com>
2021-04-18 20:57:04 +0700

    Update : Add button and section for translation

--- components/randomSurah/ayah.js
+++ components/randomSurah/ayah.js
@@ -1,7 +1,22 @@
-const Ayah = ({ styles, ayah }) => (
-  <div className={styles.ayahContainer}>
-    <h2 className={styles.ayah}>{ayah.text}</h2>
-  </div>
-);
+import { useState } from "react";
+
+const Ayah = ({ styles, ayah, text }) => {
+  const [showAyah, showText] = useState(true);
+
+  return (
+    <div className={styles.ayahContainer}>
+      {showAyah ? (
+        <h2 className={styles.ayah}>{ayah.text}</h2>
+      ) : (
+        <h2>{text.translation.id}</h2>
+      )}
+      <div className={styles.translate}>
+        <button type="button" onClick={() => showText(!showAyah)}>
+          Translate
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Ayah;
