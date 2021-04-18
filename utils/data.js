03f5647ffe985f4726282ed7a908e95428dd8f28
samx23 <skalexsong@gmail.com>
2021-04-18 19:19:37 +0700

    Updatte : Add props checking

--- utils/data.js
+++ utils/data.js
@@ -1,5 +1,10 @@
 const URLSutanlab = "https://api.quran.sutanlab.id/surah";
 const URLAlquranCloud = "https://api.alquran.cloud/v1/surah";
 
+/**
+ * @param {number} surah
+ * @param {number} ayah
+ */
+
 export const getTranslate = (surah, ayah) => `${URLSutanlab}/${surah}/${ayah}`;
 export const getAyah = (surah) => `${URLAlquranCloud}/${surah}`;
