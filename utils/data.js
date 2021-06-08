58f663ddcca144eed5750f55b61117a8b9a2804c
samx23 <skalexsong@gmail.com>
2021-06-08 18:53:47 +0700

    Update : refactor constant

--- utils/data.js
+++ utils/data.js
@@ -1,10 +1,10 @@
-const URLSutanlab = "https://api.quran.sutanlab.id/surah";
-const URLAlquranCloud = "https://api.alquran.cloud/v1/surah";
+const URLSUTANLAB = "https://api.quran.sutanlab.id/surah";
+const URLALQURANCLOUD = "https://api.alquran.cloud/v1/surah";
 
 /**
  * @param {number} surah
  * @param {number} ayah
  */
 
-export const getTranslate = (surah, ayah) => `${URLSutanlab}/${surah}/${ayah}`;
-export const getAyah = (surah) => `${URLAlquranCloud}/${surah}`;
+export const getTranslate = (surah, ayah) => `${URLSUTANLAB}/${surah}/${ayah}`;
+export const getAyah = (surah) => `${URLALQURANCLOUD}/${surah}`;
