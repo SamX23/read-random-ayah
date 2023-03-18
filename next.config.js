b84bb99e1480074621fb7a7c735fe1b98d51765c
Sami Kalammallah <skalexsong@gmail.com>
2023-03-18 17:51:21 +0700

    update<chore> : new configuration
    Signed-off-by: Sami Kalammallah <skalexsong@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-const withPWA = require("next-pwa");
+// PWA to be updated
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+});
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-  },
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["cdn.alquran.cloud"],
   },
-});
+};
